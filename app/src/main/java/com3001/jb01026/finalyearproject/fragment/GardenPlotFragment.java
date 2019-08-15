e3443334317609386c9721300c8f612b59e5d49a
DESKTOP-9QLJRQU\Jack <jack.burt1@hotmail.co.uk>
2019-08-15 13:52:01 +0100

    Fixed PlotView

--- app/src/main/java/com3001/jb01026/finalyearproject/fragment/GardenPlotFragment.java
+++ app/src/main/java/com3001/jb01026/finalyearproject/fragment/GardenPlotFragment.java
@@ -16,12 +16,19 @@ import com.johnhiott.darkskyandroidlib.RequestBuilder;
 import com.johnhiott.darkskyandroidlib.models.Request;
 import com.johnhiott.darkskyandroidlib.models.WeatherResponse;
 
+import org.w3c.dom.Text;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import androidx.appcompat.app.AlertDialog;
@@ -40,14 +47,21 @@ public class GardenPlotFragment extends Fragment {
     private GardenPlot plot;
     private SunCalculations sunCalculations = new SunCalculations();
 
+    View loadingOverlay;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_garden_plot, container, false);
 
+        loadingOverlay = view.findViewById(R.id.loading_screen);
+        loadingOverlay.setVisibility(View.VISIBLE);
+
         Bundle bundle = getArguments();
         plot = (GardenPlot) bundle.getSerializable("garden_plot");
+
+        TextView plotName = view.findViewById(R.id.plot_name);
 
 
         List<TextView> rainfallTextViews = new ArrayList<>();
@@ -59,6 +73,16 @@ public class GardenPlotFragment extends Fragment {
         rainfallTextViews.add(view.findViewById(R.id.rainfall_day_6));
         rainfallTextViews.add(view.findViewById(R.id.rainfall_day_7));
 
+        List<TextView> rainfallLabels = new ArrayList<>();
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_1_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_2_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_3_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_4_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_5_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_6_rain));
+        rainfallLabels.add(view.findViewById(R.id.day_of_week_7_rain));
+
+
         List<TextView> temperatureTextViews = new ArrayList<>();
         temperatureTextViews.add(view.findViewById(R.id.temperature_day_1));
         temperatureTextViews.add(view.findViewById(R.id.temperature_day_2));
@@ -67,6 +91,15 @@ public class GardenPlotFragment extends Fragment {
         temperatureTextViews.add(view.findViewById(R.id.temperature_day_5));
         temperatureTextViews.add(view.findViewById(R.id.temperature_day_6));
         temperatureTextViews.add(view.findViewById(R.id.temperature_day_7));
+
+        List<TextView> temperatureLabels = new ArrayList<>();
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_1_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_2_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_3_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_4_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_5_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_6_temp));
+        temperatureLabels.add(view.findViewById(R.id.day_of_week_7_temp));
 
         TextView rainToday = view.findViewById(R.id.rainfall_today);
         TextView temperatureToday = view.findViewById(R.id.temperature_today);
@@ -97,6 +130,8 @@ public class GardenPlotFragment extends Fragment {
             }
             });
 
+        plotName.setText(plot.getName());
+
         int shadeRatingValue = sunCalculations.calc_shade_rating(plot);
         shadeRating.setText(Integer.toString(shadeRatingValue));
         setColour(shadeRating, 100, shadeRatingValue);
@@ -123,10 +158,9 @@ public class GardenPlotFragment extends Fragment {
 
         ForecastApi.create("e5e6c785be3d23a995ee1ceb88afe9a0");
 
-        //request.addExcludeBlock(Request.Block.CURRENTLY);
-        List<Double> rainfallList = new ArrayList<>();
-        List<Double> temperatureList = new ArrayList<>();
-        List<Map<Long,Double>> list = new ArrayList<>();
+        Map<Long, Double> temperatureMap = new HashMap<>();
+        Map<Long, Double> rainfallMap = new HashMap<>();
+
 
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.HOUR_OF_DAY,12);
@@ -137,6 +171,31 @@ public class GardenPlotFragment extends Fragment {
 
         long unixTimestamp = todayDate.getTime()/1000;
         //unixTimestamp = 1565395200 + 43000;
+
+
+        RequestBuilder weatherToday = new RequestBuilder();
+
+        Request todayRequest = new Request();
+        todayRequest.setLat(Double.toString(gardenLocation.latitude));
+        todayRequest.setLng(Double.toString(gardenLocation.longitude));
+        todayRequest.setUnits(Request.Units.UK);
+        todayRequest.setLanguage(Request.Language.ENGLISH);
+
+        weatherToday.getWeather(todayRequest, new Callback<WeatherResponse>() {
+            @Override
+            public void success(WeatherResponse weatherResponse, Response response) {
+                rainToday.setText(Double.toString(Double.parseDouble(weatherResponse.getDaily().getData().get(0).getPrecipProbability())*100).split("\\.")[0]+"%");
+                temperatureToday.setText(Double.toString(weatherResponse.getDaily().getData().get(0).getTemperatureMax())+"\u00B0");
+            }
+
+            @Override
+            public void failure(RetrofitError retrofitError) {
+                Log.d("ERROR", "Error while calling: " + retrofitError.getUrl());
+            }
+        });
+
+
+
 
 
         for(int i = 0; i<7;i++) {
@@ -158,15 +217,21 @@ public class GardenPlotFragment extends Fragment {
                 public void success(WeatherResponse weatherResponse, Response response) {
                     //Do something
 
-                    temperatureList.add(weatherResponse.getDaily().getData().get(0).getTemperatureMax());
+//                    temperatureList.add(weatherResponse.getDaily().getData().get(0).getTemperatureMax());
+//
+//                    double rainfall = Double.parseDouble(weatherResponse.getDaily().getData().get(0).getPrecipProbability());
+//                    rainfallList.add(rainfall);
+
+
+                    long time = weatherResponse.getDaily().getData().get(0).getTime();
+                    temperatureMap.put(time,weatherResponse.getDaily().getData().get(0).getTemperatureMax());
 
                     double rainfall = Double.parseDouble(weatherResponse.getDaily().getData().get(0).getPrecipProbability());
-                    rainfallList.add(rainfall);
+                    rainfallMap.put(time, rainfall);
 
+                    if(rainfallMap.size()==7) {
 
-                    if(rainfallList.size()==7) {
-
-                        fillTextViews(temperatureList, rainfallList, temperatureTextViews, rainfallTextViews);
+                        fillTextViews(rainfallMap, temperatureMap, temperatureTextViews, rainfallTextViews, temperatureLabels, rainfallLabels);
 
                     }
                 }
@@ -183,14 +248,30 @@ public class GardenPlotFragment extends Fragment {
         return view;
     }
 
-    public void fillTextViews(List<Double> rainfallList, List<Double> temperatureList, List<TextView> temperatureTextViews, List<TextView> rainfallTextViews) {
+    public void fillTextViews(Map<Long, Double> rainfallMap, Map<Long, Double> temperatureMap, List<TextView> temperatureTextViews, List<TextView> rainfallTextViews, List<TextView> temperatureLabels, List<TextView> rainfallLabels) {
+        List sortedKeys = new ArrayList(rainfallMap.keySet());
+        Collections.sort(sortedKeys);
 
-        for(int i = 0; i < rainfallList.size(); i++) {
-            temperatureTextViews.get(i).setText(Double.toString(temperatureList.get(i)));
-            setColour(temperatureTextViews.get(i), 30, (int)Math.round(temperatureList.get(i)));
+        for(int i = 0; i < sortedKeys.size(); i++) {
 
-            rainfallTextViews.get(i).setText(Double.toString(rainfallList.get(i)));
-            setColour(rainfallTextViews.get(i), 1, (int)Math.round(rainfallList.get(i)));
+            long time = (long)sortedKeys.get(i);
+
+            Date date = new Date ();
+            date.setTime((long)time*1000);
+            LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+            double rain = rainfallMap.get(sortedKeys.get(i))*100;
+            double temp = temperatureMap.get(sortedKeys.get(i));
+
+            temperatureLabels.get(i).setText(localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT_STANDALONE, Locale.UK));
+            rainfallLabels.get(i).setText(localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT_STANDALONE, Locale.UK));
+
+            rainfallTextViews.get(i).setText(Double.toString(Math.round(rain)).split("\\.")[0]+"%");
+            temperatureTextViews.get(i).setText(Double.toString(temp)+"\u00B0");
+            setColour(rainfallTextViews.get(i), 100, (int)rain);
+            setColour(temperatureTextViews.get(i), 30, (int)temp);
+
+            loadingOverlay.setVisibility(View.GONE);
         }
     }
 
@@ -205,14 +286,15 @@ public class GardenPlotFragment extends Fragment {
     }
 
     private String formatHours(float f) {
-        Log.v("FLOAT", Float.toString(f));
+        Log.v("FLOAT f", Float.toString(f));
         double rounded = Math.round(f/0.25)*0.25;
-        Log.v("DOUBLE", Double.toString(rounded));
+        Log.v("DOUBLE rounded", Double.toString(rounded));
         //double rounded = (Math.round((f*4)/4f))*60/100;
-        String s = Double.toString(rounded);
+
+        String s = String.format("%.2f", rounded);
         System.out.println(s);
         String[] temp = s.split("\\.");
-        return temp[0] + "h" + Double.toString(Double.parseDouble(temp[1])/10*60).split("\\.")[0] + "min";
+        return temp[0] + "h" + Double.toString(Double.parseDouble(temp[1])/100*60).split("\\.")[0] + "m";
     }
 
 
